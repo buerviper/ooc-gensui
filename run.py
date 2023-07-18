cd826fafac5f93738d6a9845b1127bb5f27d3333
Mathias Micheel <49622130+buerviper@users.noreply.github.com>
2023-07-18 15:23:26 +0200

    updated run.py

--- run.py
+++ run.py
@@ -10,28 +10,46 @@ mastodon = Mastodon(
     api_base_url='botsin.space'
 )
 
+def post_status_with_image(name, spoiler_warning = "False", status = "#suikoden", sensitivity = "False", language = "en"):
+    description_file = open("images/descriptions/"+name+".yml", "r")
+    description = yaml.load(description_file, Loader=yaml.FullLoader)
+    status = description["status"]
+    spoiler_warning = description["spoiler_warning"]
+    language = description["language"]
+    sensitivity = description["sensitivity"]
+    mastodon.status_post(status, media_ids = media_list, spoiler_text = spoiler_warning, sensitive = sensitivity, language = language)
+
+# Function to prepare media with image descriptions etc.
+def media_description(media):
+    description_file = open("images/descriptions/"+media+".yml", "r")
+    description = yaml.load(description_file, Loader=yaml.FullLoader)
+    alt_text = description["description"]
+    os.chdir("images")
+    filename = media+".png"
+    post = mastodon.media_post(filename, "image/png", description = alt_text)
+    media_list.append(post["id"])
+    os.chdir("..")
+
 # Choose a random photo out of the /images folder
 photo = random.choice([x for x in os.listdir("images") if os.path.isfile(os.path.join("images", x))])
 
 # Get name of photo without ending
 name = Path("images/"+photo).stem
 
-# Check if photo has a description
-if os.path.exists("images/descriptions/"+name+".yml"):
-    description_file = open("images/descriptions/"+name+".yml", "r")
-    description = yaml.load(description_file, Loader=yaml.FullLoader)
+# Create an empty list for all the photos/media we want to add
+media_list = []
+
+# Check if photo is part of a series
+if name[-1].isdigit():
+    # Get both images
+    name_1 = name[:-1]+"1"
+    name_2 = name[:-1]+"2"
+    name_list = [name_1, name_2]
+    for x in name_list:
+        media_description(x)
+else:
+    # Get details of post
+    media_description(name)
 
 # Post a new status update
-
-def post_status_with_image(spoiler_warning, image_alt_text, post_text = "#suikoden", sensitivity = "False", language = "en"):
-    os.chdir("images")
-    media = mastodon.media_post(photo, "image/png", description = image_alt_text)
-    mastodon.status_post(post_text, media_ids=[media['id']], spoiler_text = spoiler_warning, sensitive = sensitivity, language = language)
-
-status = description["status"]
-spoiler_warning = description["spoiler_warning"]
-alt_text = description["description"]
-language = description["language"]
-sensitivity = description["sensitivity"]
-
-post_status_with_image(spoiler_warning, alt_text, status, sensitivity, language)
+post_status_with_image(name)
