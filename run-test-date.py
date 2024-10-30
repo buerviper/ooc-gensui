d59c2a85fa22cf8108c42b66a77b3ebcdbe648e7
Mathias Micheel <49622130+buerviper@users.noreply.github.com>
2024-10-30 11:48:09 +0100

    Update run-test-date.py

--- run-test-date.py
+++ run-test-date.py
@@ -11,8 +11,8 @@ mastodon = Mastodon(
 
 date = datetime.datetime.now()
 
-if int(date.strftime("%d")) == 25 and int(date.strftime("%m")) == 10:
-    post = "Heute ist weihnachten"
+if int(date.strftime("%d")) == 30 and int(date.strftime("%m")) == 11:
+    post = "morgen ist halloween"
 else:
     post = "heute ist nicht weihnachten"
 
