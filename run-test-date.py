9dadbd87b38c788d2e6b22747e3f75269b545142
Mathias Micheel <49622130+buerviper@users.noreply.github.com>
2024-10-30 11:31:33 +0100

    date test py file

---
+++ run-test-date.py
@@ -0,0 +1,23 @@
+from mastodon import Mastodon
+from pathlib import Path
+import os
+import random
+import yaml
+
+
+# Access Mastodon instance
+mastodon = Mastodon(
+    access_token=os.environ['MASTODON_ACCESS_TOKEN'],
+    api_base_url='botsin.space'
+)
+
+date = datetime.datetime.now()
+
+if int(date.strftime("%d")) == 25 and int(date.strftime("%m")) == 10:
+    post = "Heute ist weihnachten"
+else:
+    post = "heute ist nicht weihnachten"
+
+# Write a "Hello World!" post
+image = mastodon.status_post(post, # this is the only required argument. you can either give the filename directly or use the "media_file" argument.
+                            )
